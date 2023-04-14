ffbcb2809faff9336d3016d4a750ab41926b7573
Masha Vilenchyk <mariavilenchyk@gmail.com>
2023-04-15 00:09:35 +0300

    feat: add solution dream-team after disappearance(April 15, 2023 00.09)

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (members == null) {
+    return false;
+  } else {
+    let names = "";
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] == "string") {
+        names += members[i].trim()[0].toUpperCase();
+        // console.log(members[i]);
+      }
+      console.log(members.length);
+    }
+    // console.log(names.split('').sort().join(''))
+    return names.split("").sort().join("");
+  }
 }
 
 module.exports = {
